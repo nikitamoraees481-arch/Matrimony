f98c256fa89b5d01f693b81fbe7cc51d9e031abd
nikitamoraees481-arch <nikitamoraees481@gmail.com>
2026-01-15 12:15:57 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,18 +3,12 @@ const util = require("util")
 
 const conn = mysql.createConnection({
     host: "brf60mj4pwefklggzb50-mysql.services.clever-cloud.com",
-    user: "brf60mj4pwefklggzb50",
+    user: "ud79ocqrwm2gsjrt",
     password: "NkW2UBud0eBIXo364HcC",
     database: "brf60mj4pwefklggzb50"
 });
 const exe = util.promisify(conn.query).bind(conn);
 
-// db.connect((err) => {
-//     if (err) {
-//         console.log("DB Error ❌", err);
-//     } else {
-//         console.log("Database Connected ✅");
-//     }
-// });
+
 
 module.exports = exe;
