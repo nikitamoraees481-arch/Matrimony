1fa1a7063fe050a528be40fe9b3c9cbd817b9963
nikitamoraees481-arch <nikitamoraees481@gmail.com>
2026-01-15 11:29:55 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const util = require("util")
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "matrimony"
+    host: "brf60mj4pwefklggzb50-mysql.services.clever-cloud.com",
+    user: "brf60mj4pwefklggzb50",
+    password: "NkW2UBud0eBIXo364HcC",
+    database: "brf60mj4pwefklggzb50"
 });
 const exe = util.promisify(conn.query).bind(conn);
 
